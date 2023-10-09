fe13cb1d093884bd65061c05990f236985c361c5
HabibBukhori <146799892+HabibBukhori@users.noreply.github.com>
2023-10-09 15:00:33 +0800

    Day 1
    Mencetak identitas

---
+++ Identitas.java
@@ -0,0 +1,10 @@
+package DayOne;
+public class DayOne {
+  public static void main(String[] args) {
+  System.out.println("NAMA : AHMAD HABIB BUKHORI");
+  System.out.println("NIM : D0223017");
+  System.out.println("PRODI : INFORMATIKA");
+
+       }
+
+    }
