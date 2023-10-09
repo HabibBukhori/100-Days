a6cd2543f54b8381b16e57ed122d47d10b9b3119
HabibBukhori <146799892+HabibBukhori@users.noreply.github.com>
2023-10-09 15:12:29 +0800

    Identitas.java
    Cetak identitas

--- Identitas.java
+++ Identitas.java
@@ -2,8 +2,8 @@ package DayOne;
 public class DayOne {
   public static void main(String[] args) {
   System.out.println("NAMA : AHMAD HABIB BUKHORI");
-  System.out.println("NIM : D0223017");
   System.out.println("PRODI : INFORMATIKA");
+  System.out.println("ALAMAT : LEMBANG");
 
        }
 
