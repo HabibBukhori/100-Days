61b3de54aa9f3eda6a4a3bffa03ab9005d16f099
HabibBukhori <146799892+HabibBukhori@users.noreply.github.com>
2023-10-10 16:48:33 +0800

    Day Two
    Identitas ke 2 menggunakan type data primitif

---
+++ IdentitasKe2.java
@@ -0,0 +1,28 @@
+package DayTwo;
+
+public class DayTwo {
+
+public static void main(String[] args) {
+
+  // type data primitif
+  
+  byte umur = 18;
+  short tahun_lahir = 2005;
+  int kode_pos = 91413;
+  long no_wa = 6285348678024l;
+  double tinggi = 175;
+  float berat = 60;
+  char jenis_kelamin = 'L';
+  boolean status_pernikahan = false;
+  
+  System.out.println("umur: "+umur);
+  System.out.println("tahun lahir: "+tahun_lahir);
+  System.out.println("kode pos: "+kode_pos);
+  System.out.println("no wa: +"+no_wa);
+  System.out.println("tinggi: "+tinggi);
+  System.out.println("berat: "+berat);
+  System.out.println("jenis kelamin: "+jenis_kelamin);
+  System.out.println("status pernikahan: "+status_pernikahan);
+  
+  }
+}
