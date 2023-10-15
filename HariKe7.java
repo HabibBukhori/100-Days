887f0d938716f5fe84d9e9fabfde9d41ffe384c3
HabibBukhori <146799892+HabibBukhori@users.noreply.github.com>
2023-10-15 23:58:21 +0800

    HariKe7.java
    Mohon maaf jika kodingan yang saya tampilkan adalah kodingan yang mudah :(

---
+++ HariKe7.java
@@ -0,0 +1,19 @@
+package Day7;
+
+public class DaySeven {
+
+  public static void main(String[] args) {
+   // hari ke 7 ini saya akan menampilkan
+   // operator ARITMATIKA yaitu perkalian
+   // (*)
+   int a = 15;
+   int b = 4;
+   int c = a + b;
+   
+   System.out.println(c*b);
+   // hasil perkalian diatas adalah (76)
+   // mohon maaf jika hari ini saya cuman
+   // bisa menampilkan codingan yang mudah
+   
+  }
+}
