35a561285f40471c4ef4d0299995730d22528d88
HabibBukhori <146799892+HabibBukhori@users.noreply.github.com>
2023-10-19 19:59:32 +0800

    Operator perbandingan.java
    Hari ini saya menampilkan hasil latihan saya hari ini yaitu operator perbandingan. Mohon maaf jika ada kesalahan dan mohon untuk kakak mentor untuk mengoreksi codingan saya jika ada kesalahan. Terimakasih

---
+++ Operator perbandingan.java
@@ -0,0 +1,45 @@
+package Day11;
+
+public class Day11 {
+
+  public static void main(String[] args) {
+  /* Hari ke 11 ini saya akan mempelajari
+  operator perbandingan yaitu sebagai berikut
+  berikan nilai dari tipe data variabel*/
+  
+  int angka = 5;
+  int angka2 = 9;
+  
+  // perbandingan pertama(==)
+  System.out.println(angka == angka2);
+  // (false) karena 5 tidak sama dengan 9
+  
+  // perbandingan kedua(!=)
+  System.out.println(angka != angka2);
+  // (true) karna 5 tidak sama dengan 9
+  
+  // perbandingan ke3(>)
+  System.out.println(angka > angka2);
+  // (false) karna 5 tidak lebih besar dari 9 
+  
+  // perbandingan ke4(<)
+  System.out.println(angka < angka2);
+  // (true) karna 5 lebih kecil dari 9 
+  
+  // perbandingan ke5(>=)
+  System.out.println(angka >= angka2);
+  /* (false) karena 5 tidak lebih besar
+  atau tidak samadengan 9 */
+  
+  // perbandingan ke6(<=)
+  System.out.println(angka <= angka2);
+  /* (true) karna 5 lebih kecil atau
+  samadengan 9 */
+  
+   /* mungkin sampai sini saja hasil latihan
+   saya hari ini, mohon maaf jika ada kesalahan
+   dari codingan saya dan mohon untuk
+   dikoreksi juga. Terimakasih */
+     
+  }
+}
