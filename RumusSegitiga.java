5478f66e32cfd3b7119910af5f195cb456ff8bb6
HabibBukhori <146799892+HabibBukhori@users.noreply.github.com>
2023-10-24 21:43:30 +0800

    RumusSegitiga.java
    Hari ini saya latihan mencari rumus luas dan keliling lingkaran menggunakan type data aritmatika

---
+++ RumusSegitiga.java
@@ -0,0 +1,27 @@
+package Day16;
+public class RumusSegitiga {
+  public static void main(String[] args) {
+  /* Hari ke 16 ini saya menampilkan cara
+  mencari rumus luas dan keliling segitiga
+  sama sisi. Baiklah langsung saja  */
+  
+  double a, t, s, L, K;
+  /* a = alas, t = tinggi, s = sisi, 
+  L = luas, K = keliling;*/
+  a = 6;
+  t = 8;
+  s = a;
+  L = 0.5*(a*t);
+  K = 3 * s; // atau sisi + sisi + sisi
+  System.out.println("Luas Segitiga = "+L);
+  System.out.println("Keliling Segitiga = "+K);
+  
+  /* mungkin cukup sampai disini saja latihan
+  saya pada hari ini. mohon maaf jika saya
+  mengulangi codingan saya. dan mohon untuk
+  dapat mengoreksi codingan saya jika ada 
+  yang salah. TERIMA KASIH */
+
+  }
+
+}
