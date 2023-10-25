b9de671d3a8543f0e0204ce15dc090f60f1ebf4d
HabibBukhori <146799892+HabibBukhori@users.noreply.github.com>
2023-10-25 19:04:30 +0800

    SwitchCase.java
    Hari ini saya menampilkan percabangan switch case. Mohon untuk dapat dikoreksi jika terdapat codingan atau penjelasan yang salah dari saya. Terimakasih 🙏🙏

---
+++ SwitchCase.java
@@ -0,0 +1,42 @@
+package Day17;
+public class SwitchCase {
+  public static void main(String[] args) {
+  /* Hari ke 17 ini saya akan latihan menampilkan
+  percabangan yaitu switch case. */
+  int jurusan = 1;
+  switch (jurusan){
+    case 1:
+    System.out.println("Teknik Sipil");
+    break;
+    // break untuk menghentikan percabangan
+    // jika dia sudah terpenuhi
+    // sama halnya dengan if else
+    
+    case 2:
+    System.out.println("Teknik Informatika");
+    break;
+    
+    case 3:
+    System.out.println("Teknik Perancangan Wilayah dan Kota");
+    break;
+    
+    default:
+    System.out.println("Pilihan Tidak Ada");
+    /* jika nilai variabel diatas bernilai
+    angka 1 maka dia akan menampilkan case 1
+    jika nilai variabel diatas bernilai 
+    angka 2 maka dia akan menampilkan case 2
+    jika nilai variabel diatas bernilai 
+    angka 3 maka dia akan menampilkan case 3
+    dan jika nilai variabel diatas tidak bernilai
+    angka 1, 2, dan 3, Maka dia akan 
+    menampilkan default.
+    default adalah jika nilai variabel tersebut
+    tidak ada yang terpenuhi.
+    
+    Mohon dikoreksi jika ada yang salah pada
+    codingan saya atau penjelasan saya tentang switch case diatas. TERIMA KASIH */
+    
+  }
+  }
+  }
