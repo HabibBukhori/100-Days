0e2ffcc2892db09aac6d3f0c2f25b578c4088815
HabibBukhori <146799892+HabibBukhori@users.noreply.github.com>
2023-11-01 23:09:00 +0800

    WhileLoop.java
    Hari ini saya mencoba membuat program pengulangan yaitu while loop.

---
+++ WhileLoop.java
@@ -0,0 +1,27 @@
+package Day24;
+public class WhileLoop {
+  public static void main(String[] args) {
+  /* Hari ke 24 ini saya latihan membuat pengulangan
+  yaitu while loop */
+  
+  int a = 1;
+  boolean kondisi = true;
+  
+  System.out.println("=>Program Dimulai<=");
+  
+  while (kondisi){
+    System.out.println(a+". Ahmad Habib Bukhori");
+ 
+    if (a == 10){
+      kondisi = false;
+    }
+    a++;
+  }
+  System.out.println("=>Program Selesai<=");
+  /* Cukup sampai disini saja hasil latihan saya
+  pada hari ini, jika ada kekurangan dan kesalahan
+  pada kodingan saya mohon untuk dikoreksi. 
+  Terima Kasih :) */
+
+  }
+}
